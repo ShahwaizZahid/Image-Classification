b5cb49740fdbb0742e9eada766190c4a8ac31351
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-01-02 01:04:00 +0500

    refactor: refactor the detect celebrity code

--- server/utils.py
+++ server/utils.py
@@ -24,7 +24,6 @@ def classify_image(image_base64_data, file_path=None):
 
         final = combined_img.reshape(1,len_image_array).astype(float)
         final = combined_img.reshape(1,len_image_array).astype(float)
-       
         result.append({
                 'class': class_number_to_name(__model.predict(final)[0]),
                 'class_probability': np.around(__model.predict_proba(final)*100,2).tolist()[0],
@@ -86,4 +85,7 @@ def get_b64_test_image_for_virat():
 if __name__ == '__main__':
       load_saved_artifacts()
 
-      print(classify_image(get_b64_test_image_for_virat()))
+    #   print(classify_image(get_b64_test_image_for_virat()))
+
+      print(classify_image(None, "./test_images/federer2.jpg"))
+     
