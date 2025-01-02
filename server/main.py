a5aeaf0e8e8a31b05fa1578c3fa55a73e106ed59
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-01-02 20:07:12 +0500

    refactor api and auto restart server

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify # type: ignore
 import utils
-from flask_cors import CORS
+from flask_cors import CORS # type: ignore
 
 app = Flask(__name__)
 
