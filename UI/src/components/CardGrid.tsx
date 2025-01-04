a05cd63843596f86ab588e13123843e96c791998
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-01-04 22:32:28 +0500

    move array outside the fun

--- UI/src/components/CardGrid.tsx
+++ UI/src/components/CardGrid.tsx
@@ -1,35 +1,32 @@
-import React from "react";
 import Card from "./Card";
-
+export const players = [
+  {
+    name: "Lionel Messi",
+    imgSrc: "./images/messi.jpeg",
+    dataPlayer: "lionel_messi",
+  },
+  {
+    name: "Maria Sharapova",
+    imgSrc: "./images/sharapova.jpeg",
+    dataPlayer: "maria_sharapova",
+  },
+  {
+    name: "Roger Federer",
+    imgSrc: "./images/federer.jpeg",
+    dataPlayer: "roger_federer",
+  },
+  {
+    name: "Serena Williams",
+    imgSrc: "./images/serena.jpeg",
+    dataPlayer: "serena_williams",
+  },
+  {
+    name: "Virat Kohli",
+    imgSrc: "./images/virat.jpeg",
+    dataPlayer: "virat_kohli",
+  },
+];
 const CardsGrid = () => {
-  const players = [
-    {
-      name: "Lionel Messi",
-      imgSrc: "./images/messi.jpeg",
-      dataPlayer: "lionel_messi",
-    },
-    {
-      name: "Maria Sharapova",
-      imgSrc: "./images/sharapova.jpeg",
-      dataPlayer: "maria_sharapova",
-    },
-    {
-      name: "Roger Federer",
-      imgSrc: "./images/federer.jpeg",
-      dataPlayer: "roger_federer",
-    },
-    {
-      name: "Serena Williams",
-      imgSrc: "./images/serena.jpeg",
-      dataPlayer: "serena_williams",
-    },
-    {
-      name: "Virat Kohli",
-      imgSrc: "./images/virat.jpeg",
-      dataPlayer: "virat_kohli",
-    },
-  ];
-
   return (
     <div className="grid grid-cols-5 gap-4 mb-8">
       {players.map((player, index) => (
